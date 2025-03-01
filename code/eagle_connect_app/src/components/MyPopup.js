3ff8cc56283374da1020076d271c5720e76a1669
Dylan Hardy <dylanmhardy10@yahoo.com>
2025-03-01 00:38:32 -0500

    updated the popup to the specifaction talked about in meeting

--- code/eagle_connect_app/src/components/MyPopup.js
+++ code/eagle_connect_app/src/components/MyPopup.js
@@ -8,16 +8,51 @@ const MyPopup = ({ isOpen, closePopup }) => {
     <Popup open={isOpen} closeOnDocumentClick onClose={closePopup}>
       <h2>Create A Class</h2>
       <form>
-        <label htmlFor="className">Class Name: </label>
-        <input type="text" id="className" className="input-with-padding" />
+        <label htmlFor="className">Class Number: </label>
 
-        <label htmlFor="classNumber">Class Number: </label>
+        <div class="className">
+          <input type="text" id="className" className="input-small" />
+          <label htmlFor="className3" class="dash">
+            -
+          </label>
+
+          <input
+            type="number"
+            id="className"
+            className="classNameIn"
+            min="0"
+            max="999"
+          />
+
+          <select>
+            <option value="option1"></option>
+            <option value="option2">L</option>
+            <option value="option3">C</option>
+            <option value="option3">D</option>
+          </select>
+
+          <label htmlFor="className3" class="dash">
+            -
+          </label>
+          <input
+            type="number"
+            id="className"
+            className="classNameIn"
+            min="0"
+            max="999"
+          />
+          <select>
+            <option value="option1"></option>
+            <option value="option2">UR</option>
+          </select>
+        </div>
+
+        <label htmlFor="classNumber">Class Name: </label>
         <input type="text" id="classNumber" className="input-with-padding" />
 
-        <label htmlFor="professorName">Professor Name: </label>
-        <input type="text" id="professorName" className="input-with-padding" />
-
-        <button type="submit">Add</button>
+        <button type="submit" class="add-button">
+          Add
+        </button>
       </form>
     </Popup>
   );
