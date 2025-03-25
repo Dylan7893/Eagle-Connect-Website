ebc386374c89b7529177a6a4b9a8b77a51f99971
Dylan Hardy <dylanmhardy10@yahoo.com>
2025-03-24 21:16:14 -0400

    replaced logos

--- code/eagle_connect_app/src/pages/WelcomePage.jsx
+++ code/eagle_connect_app/src/pages/WelcomePage.jsx
@@ -1,43 +1,51 @@
-import homeStyle from '../design/homepageStyle.css';
-import eagleLogo from '../design/Morehead_State_Eagles_logo.svg.png';
-import { useNavigate } from 'react-router-dom';
+import homeStyle from "../design/homepageStyle.css";
+import eagleLogo from "../design/logo.png";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 const WelcomePage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const navigateClick = () => {
+    navigate("/app");
+  };
 
-    const navigateClick = () => {
-        navigate('/app');
-      };
-      
-    return (
-        <>
-            <header>
-                <nav class="navigationHeader">
+  return (
+    <>
+      <header>
+        <nav class="navigationHeader">
+          <div class="logo">
+            <img
+              src={eagleLogo}
+              style={homeStyle}
+              alt="Eagle Logo"
+              class="logo-image"
+              EAGLE
+              CONNECT
+            ></img>
+          </div>
 
-                    <div class="logo">
-                        <img src={eagleLogo} style={homeStyle} alt="Eagle Logo" class="logo-image"
-                            EAGLE CONNECT></img>
-                    </div>
+          <ul class="navigation-links">
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <button class="get-started" onClick={navigateClick}>
+                Get Started
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-                    <ul class="navigation-links">
-                        <li><a href="#">About</a></li>
-                        <li><button class="get-started" onClick={navigateClick}>Get Started</button></li>
-                    </ul>
-                </nav>
-            </header>
-
-            <main>
-                <section class="main-display">
-                    <h1>Welcome to Eagle Connect</h1>
-                    <p>Connect with colleagues today</p>
-                </section>
-            </main>
-        </>
-    );
+      <main>
+        <section class="main-display">
+          <h1>Welcome to Eagle Connect</h1>
+          <p>Connect with colleagues today</p>
+        </section>
+      </main>
+    </>
+  );
 };
-
-
 
 export default WelcomePage;
